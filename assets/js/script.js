1d3bd03363c2f5a69d161ddbcc4e945fe4e37de2
Chris <157438747+ChrisVulpine@users.noreply.github.com>
2024-04-13 23:30:27 -0400

    added additional styling to elements. added animations, created cards for 5 day forecast, modified colors.

--- assets/js/script.js
+++ assets/js/script.js
@@ -17,6 +17,10 @@ var queryString;
 var forecastURL; 
 
 var day1Card = document.querySelector('#day1Card');
+var day2Card = document.querySelector('#day2Card');
+var day3Card = document.querySelector('#day3Card');
+var day4Card = document.querySelector('#day4Card');
+var day5Card = document.querySelector('#day5Card');
 
 // Add timezone plugins to day.js
 dayjs.extend(window.dayjs_plugin_utc);
@@ -116,20 +120,22 @@ function getTodayWeather(event) {
     console.log(forecastURL);
     currentWeather.textContent = '';
 
-    var cityNameEl = document.createElement('h1');
+    var cityNameEl = document.createElement('h2');
     var weatherIconEl = document.createElement('img');
     var tempEl = document.createElement('h3');
     var windEl = document.createElement('h3');
     var humidityEl = document.createElement('h3');
   
 
-    cityNameEl.textContent = `Current Weather: ${cityName} (${date})`;
+    cityNameEl.textContent = `${cityName} (${date})`;
     weatherIconEl.src= `https://openweathermap.org/img/wn/${weatherIcon}@2x.png`
     tempEl.textContent = `Temp: ${temp} °F`;
     windEl.textContent = `Wind Speed: ${wind} MPH`;
     humidityEl.textContent = `Humidity: ${humidity}%`;
 
     currentWeather.append(cityNameEl);
+    currentWeather.setAttribute('class', 'whiteborderOn col-9 grid text-center uk-animation-slide-bottom');
+
     currentWeather.append(weatherIconEl);
     currentWeather.append(tempEl);
     currentWeather.append(windEl);
@@ -189,19 +195,19 @@ function forecast(lat, lon) {
     var today = dayjs()
 
     // Day 1 //
-    var day1Date = today.add(1, 'day').format('dddd M/D/YYYY');
+    var day1Date = today.add(1, 'day').format('ddd M/D/YYYY');
 
     // Day 2 //
-    var day2Date = today.add(2, 'day').format('dddd M/D/YYYY');
+    var day2Date = today.add(2, 'day').format('ddd M/D/YYYY');
 
     // Day 3 //
-    var day3Date = today.add(3, 'day').format('dddd M/D/YYYY');
+    var day3Date = today.add(3, 'day').format('ddd M/D/YYYY');
 
     // Day 4 //
-    var day4Date = today.add(4, 'day').format('dddd M/D/YYYY');
+    var day4Date = today.add(4, 'day').format('ddd M/D/YYYY');
 
     // Day 5 //
-    var day5Date = today.add(5, 'day').format('dddd M/D/YYYY');
+    var day5Date = today.add(5, 'day').format('ddd M/D/YYYY');
 
 
     // Day 1 //
@@ -239,41 +245,42 @@ function forecast(lat, lon) {
     var fiveDayForecastTitle = document.createElement('h1');
     // Day 1 //
 
-    var day1DateEl = document.createElement('h2');
+    var day1DateEl = document.createElement('h3');
     var day1IconEl = document.createElement('img');
-    var day1TempEl = document.createElement('h3');
-    var day1WindEl = document.createElement('h3');
-    var day1HumidEl = document.createElement('h3');
+    var day1TempEl = document.createElement('p');
+    var day1WindEl = document.createElement('p');
+    var day1HumidEl = document.createElement('p');
 
     // Day 2 //
-    var day2DateEl = document.createElement('h2');
+    var day2DateEl = document.createElement('h3');
     var day2IconEl = document.createElement('img');
-    var day2TempEl = document.createElement('h3');
-    var day2WindEl = document.createElement('h3');
-    var day2HumidEl = document.createElement('h3');
+    var day2TempEl = document.createElement('p');
+    var day2WindEl = document.createElement('p');
+    var day2HumidEl = document.createElement('p');
 
     // Day 3 //
-    var day3DateEl = document.createElement('h2');
+    var day3DateEl = document.createElement('h3');
     var day3IconEl = document.createElement('img');
-    var day3TempEl = document.createElement('h3');
-    var day3WindEl = document.createElement('h3');
-    var day3HumidEl = document.createElement('h3');
+    var day3TempEl = document.createElement('p');
+    var day3WindEl = document.createElement('p');
+    var day3HumidEl = document.createElement('p');
 
     // Day 4 //
-    var day4DateEl = document.createElement('h2');
+    var day4DateEl = document.createElement('h3');
     var day4IconEl = document.createElement('img');
-    var day4TempEl = document.createElement('h3');
-    var day4WindEl = document.createElement('h3');
-    var day4HumidEl = document.createElement('h3');
+    var day4TempEl = document.createElement('p');
+    var day4WindEl = document.createElement('p');
+    var day4HumidEl = document.createElement('p');
 
     // Day 5 //
-    var day5DateEl = document.createElement('h2');
+    var day5DateEl = document.createElement('h3');
     var day5IconEl = document.createElement('img');
-    var day5TempEl = document.createElement('h3');
-    var day5WindEl = document.createElement('h3');
-    var day5HumidEl = document.createElement('h3');    
+    var day5TempEl = document.createElement('p');
+    var day5WindEl = document.createElement('p');
+    var day5HumidEl = document.createElement('p');    
 
-    fiveDayForecastTitle.textContent = `Five Day Forecast:`
+    // fiveDayForecastTitle.textContent = `Five Day Forecast:`
+
     // Day 1 //
     day1DateEl.textContent = `${day1Date}`;
     day1IconEl.src= `https://openweathermap.org/img/wn/${day1Icon}@2x.png`;
@@ -310,42 +317,57 @@ function forecast(lat, lon) {
     day5HumidEl.textContent = `Humidity: ${day5Humid}%`;    
 
     forecastContainer.textContent = '';
+    day1Card.textContent = '';
+    day2Card.textContent = '';
+    day3Card.textContent = '';
+    day4Card.textContent = '';
+    day5Card.textContent = '';
 
-    forecastContainer.append(fiveDayForecastTitle);
+    forecastContainer.append(day1Card);
+    forecastContainer.append(day2Card);
+    forecastContainer.append(day3Card);
+    forecastContainer.append(day4Card);
+    forecastContainer.append(day5Card);
+
     // Day 1 //
-    forecastContainer.append(day1DateEl);
-    forecastContainer.append(day1IconEl);
-    forecastContainer.append(day1TempEl);
-    forecastContainer.append(day1WindEl);
-    forecastContainer.append(day1HumidEl);
+    day1Card.setAttribute('class', 'fiveDayStyle text-center uk-animation-slide-left');
+    day1Card.append(day1DateEl);
+    day1Card.append(day1IconEl);
+    day1Card.append(day1TempEl);
+    day1Card.append(day1WindEl);
+    day1Card.append(day1HumidEl);
 
     // Day 2 //
-    forecastContainer.append(day2DateEl);
-    forecastContainer.append(day2IconEl);
-    forecastContainer.append(day2TempEl);
-    forecastContainer.append(day2WindEl);
-    forecastContainer.append(day2HumidEl);
+    day2Card.setAttribute('class', 'fiveDayStyle text-center uk-animation-slide-left');
+    day2Card.append(day2DateEl);
+    day2Card.append(day2IconEl);
+    day2Card.append(day2TempEl);
+    day2Card.append(day2WindEl);
+    day2Card.append(day2HumidEl);
 
     // Day 3 //
-    forecastContainer.append(day3DateEl);
-    forecastContainer.append(day3IconEl);
-    forecastContainer.append(day3TempEl);
-    forecastContainer.append(day3WindEl);
-    forecastContainer.append(day3HumidEl);
+    day3Card.setAttribute('class', 'fiveDayStyle text-center uk-animation-slide-left');
+    day3Card.append(day3DateEl);
+    day3Card.append(day3IconEl);
+    day3Card.append(day3TempEl);
+    day3Card.append(day3WindEl);
+    day3Card.append(day3HumidEl);
 
     // Day 4 //
-    forecastContainer.append(day4DateEl);
-    forecastContainer.append(day4IconEl);
-    forecastContainer.append(day4TempEl);
-    forecastContainer.append(day4WindEl);
-    forecastContainer.append(day4HumidEl);
+    day4Card.setAttribute('class', 'fiveDayStyle text-center uk-animation-slide-left');
+    day4Card.append(day4DateEl);
+    day4Card.append(day4IconEl);
+    day4Card.append(day4TempEl);
+    day4Card.append(day4WindEl);
+    day4Card.append(day4HumidEl);
 
     // Day 5 //
-    forecastContainer.append(day5DateEl);
-    forecastContainer.append(day5IconEl);
-    forecastContainer.append(day5TempEl);
-    forecastContainer.append(day5WindEl);
-    forecastContainer.append(day5HumidEl); 
+    day5Card.setAttribute('class', 'fiveDayStyle text-center uk-animation-slide-left');
+    day5Card.append(day5DateEl);
+    day5Card.append(day5IconEl);
+    day5Card.append(day5TempEl);
+    day5Card.append(day5WindEl);
+    day5Card.append(day5HumidEl); 
     
     
 
@@ -413,7 +435,7 @@ function displayHistoryBtn(search) {
 
     
 
-var cityNameEl = document.createElement('h1');
+var cityNameEl = document.createElement('h2');
 var weatherIconEl = document.createElement('img');
 var tempEl = document.createElement('h3');
 var windEl = document.createElement('h3');
